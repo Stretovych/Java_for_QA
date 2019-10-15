39fd031fb243c9a467e1ac85b46b3f6504c50f5a
stretovyych <box.antona@gmail.com>
2019-10-15 15:29:57 +0300

    Initial commit

---
+++ src/test/java/Package_Java_for_QA/java_lessons.java
@@ -0,0 +1,11 @@
+package Package_Java_for_QA;
+
+public class java_lessons {
+
+    public static void main (String[]args) {
+        System.out.println("Hello, world!");
+    }
+    public static void hello() {
+
+    }
+}
